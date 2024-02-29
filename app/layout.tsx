c25a67a6e8577cdfb36cd0dd06a675a0f354f74c
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2024-02-29 11:58:17 +0100

    pwa added

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="manifest" href="/manifest.json"></link>
       <link rel="icon" sizes="128x128" href="favicon.ico"></link>
       <body className={inter.className} cz-shortcut-listen='true'>{children}</body>
     </html>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withPWA = require('next-pwa')
+const nextConfig = {
+};
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === 'development'
+    }
+})
 
 export default nextConfig;
