deeb27f05e257ecd156ddf70dfebf670b1f96b01
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2024-02-29 12:10:35 +0100

    config update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-    reactStrictMode: true,
-    pwa: {
-        dest: 'public',
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === 'development'
-    }
+const withPWA = require('next-pwa')({
+  dest: 'public'
 })
 
+module.exports = withPWA({
+  // next.js config
+})
