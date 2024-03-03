1fc18fa36b14a1d1de86483ca6b07d0b4274babe
steve958 <stevenex958@gmail.com>
2024-03-03 19:25:13 +0100

    color update

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -208,7 +208,7 @@ export default function CalendarComponent() {
     <>
       {configClicked && (
         <div
-          className="drawer flex flex-col absolute bottom-0 left-0 bg-white rounded w-full z-10 p-5 h-64"
+          className="drawer flex flex-col absolute bottom-0 dark:bg-gray-700 left-0 bg-white rounded w-full z-10 p-5 h-64"
           id={closeClicked ? 'close-drawer' : ''}
         >
           <svg
@@ -242,7 +242,7 @@ export default function CalendarComponent() {
                 />
                 <label
                   htmlFor="default-radio-2"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  className="ms-2 text-sm font-medium text-black"
                 >
                   Prilagodi tamnoj temi
                 </label>
@@ -267,7 +267,7 @@ export default function CalendarComponent() {
                       placeholder=" "
                       required
                     />
-                    <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                    <label className="peer-focus:font-medium absolute text-sm text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                       Šifra
                     </label>
                     <div className="text-center">
